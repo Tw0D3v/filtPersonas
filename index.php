89d3acf1a013073346fd6adce5e168f7f99df63f
Brando felipe cortes <72344019+Tw0D3v@users.noreply.github.com>
2022-10-13 17:20:27 +0000

    fix terminet

--- index.php
+++ index.php
@@ -11,49 +11,36 @@ $personas = array(
   array('nombre'=>'Monica araujo', 'edad'=> 65, 'genero'=>'f'),
   array('nombre'=> 'Mauricio Coll Menendez','edad'=> 72,'genero'=>'m'),
   array('nombre'=> 'Milena Andrea', 'edad'=> 21,'genero'=>'f'),
-  array('nombre'=>'Leire Sebastián Barceló','edad'=> 41,'genero'=>'f'),
+  array('nombre'=>'Leire Sebastián Barceló','edad'=> 14,'genero'=>'f'),
   array('nombre'=> 'Ciro Vilanova Bautista', 'edad'=>36,'genero'=>'f'),
   array('nombre'=>'Gertrudis Pino','edad'=>67,'genero'=>'f'),
   array('nombre'=> 'Raquel Barroso Maldonado','edad'=>35,'genero'=>'f'),
-  array('nombre'=> 'Carmen jurado','edad'=>14,'genero'=>'f'),
-  array('nombre'=>'Serafina Villena Gaya','edad'=>15,'genero'=>'f'),
+  array('nombre'=> 'Carmen jurado','edad'=>15,'genero'=>'f'),
+  array('nombre'=>'Serafina Villena Gaya','edad'=>14,'genero'=>'f'),
   array('nombre'=>'Emigdio Cuadrado Ríos','edad'=>56,'genero'=>'m'),
   array('nombre'=>'lupe julia casas','edad'=>18,'genero'=>'f'),);
 
   $mujeres = array();
-  $hombres = array();
-  $edadesM = array();
 
- 
-foreach($personas as $persona){
-    if($persona['genero'] == 'm'){
-      array_push($hombres,$persona);  
-    }
-  }
 
- foreach($personas as $persona){
-    if($persona['genero'] == 'f'){
-      array_push($mujeres,$persona);
-    }
-  }
+ //foreach($personas as $persona){
+   // if($persona['genero'] == 'f'){
+     // array_push($mujeres,$persona);
+  //  }
+  //}
 
-echo "<h1>Mujeres mayores a 35</h1>";
-$min =1;
-foreach($mujeres as $edades){
-$edades['edad'];
-  $e;
-  for($i=0; $i < count($edades); $i++){
-    if($min<$edades[$i++]){
-      echo $min=$edades[$i++];
-    }else if ($min>$edades[$i++]){
-        echo $edades[$i++]=$min;
-    }else{
-        echo $min;
-    }
-  
+echo "<h1>Mujer de menor edad</h1>";
+usort($mujeres, function($edadMujeres, $edad) {
+	return $edadMujeres['edad'] > $edad["edad"] ? 1 : -1;
+});
+$minEdad = $mujeres[0]["edad"];
 
-  }
-  }
+$filtradoEdades = array_filter($mujeres, function($edadComparar) use ($minEdad) {
+	return $edadComparar["edad"] === $minEdad;
+});
+
+print_r($filtradoEdades);
+echo "<br>";
 
 
 
